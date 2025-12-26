a479aa40e690ed5f8f23a2b071cebca181e1f697
bhuvan-365 <bhuvanbhattarai07@gmail.com>
2025-12-26 15:22:36 +0545

    data modeling ecom

---
+++ 3-data-modeling/model/ecommerce/order.models.js
@@ -0,0 +1,40 @@
+import { Timestamp } from 'bson';
+import mongoose from 'mongoose';
+
+const orderItemsSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    orderItems: {
+      type: [orderItemSchema],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['PENDING', 'CANCELLED', 'DELIVERED'],
+      default: 'PENDING',
+    },
+  },
+  { Timestamp: true }
+);
+
+export const Order = mongoose.model('Order', orderSchema);
